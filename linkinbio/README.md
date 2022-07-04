76472bf9369791296988449443c9678135f992d2
Jins <mrjk05@users.noreply.github.com>
2022-07-04 21:56:44 +1000

    Updated from Publii

--- linkinbio/README.md
+++
@@ -1,2 +0,0 @@
-# jkadwood
-personal site
--- linkinbio/script.js
+++
@@ -1,5 +0,0 @@
-console.log('Hey there, curious developer! I see you peeking ;)');
-
-console.log(
-	'Feel free to check out all the code on my github: https://github.com/stephjs'
-);
